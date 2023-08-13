511ae982f3a1993fed864892b444620a922979d1
Alexandre Wetzel <alexwtz@gmail.com>
2023-08-13 17:10:05 +0200

    ajout rtc + dcf77

--- servo/servo.ino
+++ servo/servo.ino
@@ -1,261 +1,320 @@
-/*************************************************** 
-  This is an example for our Adafruit 16-channel PWM & Servo driver
-  Servo test - this will drive 8 servos, one after the other on the
-  first 8 pins of the PCA9685
-
-  Pick one up today in the adafruit shop!
-  ------> http://www.adafruit.com/products/815
-  
-  These drivers use I2C to communicate, 2 pins are required to  
-  interface.
-
-  Adafruit invests time and resources providing this open source code, 
-  please support Adafruit and open-source hardware by purchasing 
-  products from Adafruit!
-
-  Written by Limor Fried/Ladyada for Adafruit Industries.  
-  BSD license, all text above must be included in any redistribution
- ****************************************************/
-
 #include <Wire.h>
 #include <Adafruit_PWMServoDriver.h>
+
+// Date and time functions using a DS1307 RTC connected via I2C and Wire lib
+#include "RTClib.h"
+
+//DCF77
+#include <Arduino.h>
+#include "decodeurDCF77.h"
+
+
+RTC_DS1307 rtc;
+//int RST_PIN = 8;
+//int RELAY_PIN = 9;
+
+const uint8_t PIN_DCF77 = 3;
+char daysOfTheWeek[7][12] = { "Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday" };
 
 // called this way, it uses the default address 0x40
 Adafruit_PWMServoDriver pwm1 = Adafruit_PWMServoDriver(0x40);
 Adafruit_PWMServoDriver pwm2 = Adafruit_PWMServoDriver(0x41);
 Adafruit_PWMServoDriver pwm3 = Adafruit_PWMServoDriver(0x42);
-Adafruit_PWMServoDriver pwm[] = {pwm1,pwm2,pwm3};
+Adafruit_PWMServoDriver pwm[] = { pwm1, pwm2, pwm3 };
 
 uint16_t digitPWMCurrent[10][6][2];
-uint16_t digitPWM[10][6][2]={
-//0
-{
-  //servo0 -> {aiguille0, aiguille2}
-  {0,0},
-  //servo1
-  {0,0},
-  {0,0},
-  {0,0},
-  {0,0},
-  {0,0}
-},
-//1
-{
-  {0,0},
-  {0,0},
-  {0,0},
-  {0,0},
-  {0,0},
-  {0,0}  
-},
-//2
-{
-  {0,0},
-  {0,0},
-  {0,0},
-  {0,0},
-  {0,0},
-  {0,0}  
-},
-//3
-{
-  {0,0},
-  {0,0},
-  {0,0},
-  {0,0},
-  {0,0},
-  {0,0}  
-},
-//4
-{
-  {0,0},
-  {0,0},
-  {0,0},
-  {0,0},
-  {0,0},
-  {0,0}  
-},
-//5
-{
-  {0,0},
-  {0,0},
-  {0,0},
-  {0,0},
-  {0,0},
-  {0,0}  
-},
-//6
-{
-  {0,0},
-  {0,0},
-  {0,0},
-  {0,0},
-  {0,0},
-  {0,0}  
-},
-//7
-{
-  {0,0},
-  {0,0},
-  {0,0},
-  {0,0},
-  {0,0},
-  {0,0}  
-},
-//8
-{
-  {0,0},
-  {0,0},
-  {0,0},
-  {0,0},
-  {0,0},
-  {0,0}  
-},
-//9
-{
-  {0,0},
-  {0,0},
-  {0,0},
-  {0,0},
-  {0,0},
-  {0,0}  
-}
-};
-
-uint8_t screenConfig[4][6][2][2]={
+uint16_t digitPWM[10][6][2] = {
+  //0
   {
-    {{0,0},{0,1}},
-    {{0,2},{0,3}},
-    {{0,4},{0,5}},
-    {{0,6},{0,7}},
-    {{0,8},{0,9}},
-    {{0,10},{0,11}}
-  },{
-    {{0,12},{0,13}},
-    {{0,14},{0,15}},
-    {{1,0},{1,1}},
-    {{1,2},{1,3}},
-    {{1,4},{1,5}},
-    {{1,6},{1,7}}
-  },{
-    {{1,8},{1,9}},
-    {{1,10},{1,11}},
-    {{1,12},{1,13}},
-    {{1,14},{1,15}},
-    {{2,2},{2,3}},
-    {{2,0},{2,1}},
-  },{
-    {{2,4},{2,5}},
-    {{2,6},{2,7}},
-    {{2,8},{2,9}},
-    {{2,10},{2,11}},
-    {{2,12},{2,13}},
-    {{2,14},{2,15}}
-  }
+    //servo0 -> {aiguille0, aiguille2}
+    { 0, 0 },
+    //servo1
+    { 0, 0 },
+    { 0, 0 },
+    { 0, 0 },
+    { 0, 0 },
+    { 0, 0 } },
+  //1
+  {
+    { 0, 0 },
+    { 0, 0 },
+    { 0, 0 },
+    { 0, 0 },
+    { 0, 0 },
+    { 0, 0 } },
+  //2
+  {
+    { 0, 0 },
+    { 0, 0 },
+    { 0, 0 },
+    { 0, 0 },
+    { 0, 0 },
+    { 0, 0 } },
+  //3
+  {
+    { 0, 0 },
+    { 0, 0 },
+    { 0, 0 },
+    { 0, 0 },
+    { 0, 0 },
+    { 0, 0 } },
+  //4
+  {
+    { 0, 0 },
+    { 0, 0 },
+    { 0, 0 },
+    { 0, 0 },
+    { 0, 0 },
+    { 0, 0 } },
+  //5
+  {
+    { 0, 0 },
+    { 0, 0 },
+    { 0, 0 },
+    { 0, 0 },
+    { 0, 0 },
+    { 0, 0 } },
+  //6
+  {
+    { 0, 0 },
+    { 0, 0 },
+    { 0, 0 },
+    { 0, 0 },
+    { 0, 0 },
+    { 0, 0 } },
+  //7
+  {
+    { 0, 0 },
+    { 0, 0 },
+    { 0, 0 },
+    { 0, 0 },
+    { 0, 0 },
+    { 0, 0 } },
+  //8
+  {
+    { 0, 0 },
+    { 0, 0 },
+    { 0, 0 },
+    { 0, 0 },
+    { 0, 0 },
+    { 0, 0 } },
+  //9
+  {
+    { 0, 0 },
+    { 0, 0 },
+    { 0, 0 },
+    { 0, 0 },
+    { 0, 0 },
+    { 0, 0 } }
 };
 
-// you can also call it with a different address you want
-//Adafruit_PWMServoDriver pwm = Adafruit_PWMServoDriver(0x41);
-// you can also call it with a different address and I2C interface
-//Adafruit_PWMServoDriver pwm = Adafruit_PWMServoDriver(0x40, Wire);
+uint8_t screenConfig[4][6][2][2] = {
+  { { { 0, 0 }, { 0, 1 } },
+    { { 0, 2 }, { 0, 3 } },
+    { { 0, 4 }, { 0, 5 } },
+    { { 0, 6 }, { 0, 7 } },
+    { { 0, 8 }, { 0, 9 } },
+    { { 0, 10 }, { 0, 11 } } },
+  { { { 0, 12 }, { 0, 13 } },
+    { { 0, 14 }, { 0, 15 } },
+    { { 1, 0 }, { 1, 1 } },
+    { { 1, 2 }, { 1, 3 } },
+    { { 1, 4 }, { 1, 5 } },
+    { { 1, 6 }, { 1, 7 } } },
+  {
+    { { 1, 8 }, { 1, 9 } },
+    { { 1, 10 }, { 1, 11 } },
+    { { 1, 12 }, { 1, 13 } },
+    { { 1, 14 }, { 1, 15 } },
+    { { 2, 2 }, { 2, 3 } },
+    { { 2, 0 }, { 2, 1 } },
+  },
+  { { { 2, 4 }, { 2, 5 } },
+    { { 2, 6 }, { 2, 7 } },
+    { { 2, 8 }, { 2, 9 } },
+    { { 2, 10 }, { 2, 11 } },
+    { { 2, 12 }, { 2, 13 } },
+    { { 2, 14 }, { 2, 15 } } }
+};
 
-// Depending on your servo make, the pulse width min and max may vary, you 
-// want these to be as small/large as possible without hitting the hard stop
-// for max range. You'll have to tweak them as necessary to match the servos you
-// have!
-#define SERVOMIN  150 // This is the 'minimum' pulse length count (out of 4096)
-#define SERVOMAX  600 // This is the 'maximum' pulse length count (out of 4096)
-#define USMIN  600 // This is the rounded 'minimum' microsecond length based on the minimum pulse of 150
-#define USMAX  2400 // This is the rounded 'maximum' microsecond length based on the maximum pulse of 600
-#define SERVO_FREQ 50 // Analog servos run at ~50 Hz updates
-
-// our servo # counter
-uint8_t servonum = 0;
+#define SERVOMIN 150   // This is the 'minimum' pulse length count (out of 4096)
+#define SERVOMAX 600   // This is the 'maximum' pulse length count (out of 4096)
+#define USMIN 600      // This is the rounded 'minimum' microsecond length based on the minimum pulse of 150
+#define USMAX 2400     // This is the rounded 'maximum' microsecond length based on the maximum pulse of 600
+#define SERVO_FREQ 50  // Analog servos run at ~50 Hz updates
 
 void setup() {
   Serial.begin(9600);
   Serial.println("8 channel Servo test!");
 
-  for(uint8_t i = 0; i++; i<3){
+  for (uint8_t i = 0; i++; i < 3) {
     pwm[i].begin();
-    /*
-    * In theory the internal oscillator (clock) is 25MHz but it really isn't
-    * that precise. You can 'calibrate' this by tweaking this number until
-    * you get the PWM update frequency you're expecting!
-    * The int.osc. for the PCA9685 chip is a range between about 23-27MHz and
-    * is used for calculating things like writeMicroseconds()
-    * Analog servos run at ~50 Hz updates, It is importaint to use an
-    * oscilloscope in setting the int.osc frequency for the I2C PCA9685 chip.
-    * 1) Attach the oscilloscope to one of the PWM signal pins and ground on
-    *    the I2C PCA9685 chip you are setting the value for.
-    * 2) Adjust setOscillatorFrequency() until the PWM update frequency is the
-    *    expected value (50Hz for most ESCs)
-    * Setting the value here is specific to each individual I2C PCA9685 chip and
-    * affects the calculations for the PWM update frequency. 
-    * Failure to correctly set the int.osc value will cause unexpected PWM results
-    */
     pwm[i].setOscillatorFrequency(27000000);
     pwm[i].setPWMFreq(SERVO_FREQ);  // Analog servos run at ~50 Hz updates
   }
-  delay(10);
+
+  pinMode(PIN_DCF77, INPUT);
+
+  if (!rtc.begin()) {
+    Serial.println("Couldn't find RTC");
+    Serial.flush();
+  }
+  Serial.println("RTC found");
+
+
+  if (!rtc.isrunning()) {
+    Serial.println("RTC is NOT running, let's set the time!");
+    // When time needs to be set on a new device, or after a power loss, the
+    // following line sets the RTC to the date & time this sketch was compiled
+    //rtc.adjust(DateTime(F(__DATE__), F(__TIME__)));
+    // This line sets the RTC with an explicit date & time, for example to set
+    // January 1, 2023 at 0am you would call:
+    rtc.adjust(DateTime(2023, 1, 1, 0, 0, 0));
+  }
+
+  // When time needs to be re-set on a previously configured device, the
+  // following line sets the RTC to the date & time this sketch was compiled
+  // rtc.adjust(DateTime(F(__DATE__), F(__TIME__)));
+  // This line sets the RTC with an explicit date & time, for example to set
+  // January 21, 2014 at 3am you would call:
+  //rtc.adjust(DateTime(2021, 1, 21, 3, 0, 0));
+
+  Serial.println("Demo decodage signal DCF77\n");
+  Serial.println("Recherche pulsation...");
+  displayTime();
+
+  delay(1);
 }
 
-uint16_t getNextPos(uint8_t digit, uint8_t servo, uint8_t aiguille){
+void displayTime() {
+
+  Serial.println();
+  DateTime now = rtc.now();
+  Serial.print(now.year(), DEC);
+  Serial.print('/');
+  if (now.month() < 10) {
+    Serial.print("0");
+  }
+  Serial.print(now.month(), DEC);
+  Serial.print('/');
+  if (now.day() < 10) {
+    Serial.print("0");
+  }
+  Serial.print(now.day(), DEC);
+  Serial.print(daysOfTheWeek[now.dayOfTheWeek()]);
+  if (now.hour() < 10) {
+    Serial.print("0");
+  }
+  Serial.print(now.hour(), DEC);
+  Serial.print(':');
+  if (now.minute() < 10) {
+    Serial.print("0");
+  }
+  Serial.print(now.minute(), DEC);
+  Serial.print(':');
+  if (now.second() < 10) {
+    Serial.print("0");
+  }
+  Serial.print(now.second(), DEC);
+}
+
+void Serial_printDCF77() {
+  switch (decodeurDCF77.joursem()) {
+    case 0: Serial.println("(vide)"); return;
+    case 1: Serial.print("Lundi"); break;
+    case 2: Serial.print("Mardi"); break;
+    case 3: Serial.print("Mercredi"); break;
+    case 4: Serial.print("Jeudi"); break;
+    case 5: Serial.print("Vendredi"); break;
+    case 6: Serial.print("Samedi"); break;
+    case 7: Serial.print("Dimanche"); break;
+  }
+  Serial.print(' ');
+  Serial_print99(decodeurDCF77.jour());
+  Serial.print('/');
+  Serial_print99(decodeurDCF77.mois());
+  Serial.print("/20");
+  Serial_print99(decodeurDCF77.annee());
+  Serial.print(' ');
+  Serial_print99(decodeurDCF77.heure());
+  Serial.print(':');
+  Serial_print99(decodeurDCF77.minute());
+  Serial.print(' ');
+  if (decodeurDCF77.heure_ete()) {
+    Serial.print("(heure d'ete)");
+  } else {
+    Serial.print("(heure d'hiver)");
+  }
+
+  DateTime now = rtc.now();
+  DateTime dcf77 = DateTime(decodeurDCF77.annee(), decodeurDCF77.mois(), decodeurDCF77.jour(), decodeurDCF77.heure(), decodeurDCF77.minute(), 0);
+
+  if (abs(now.unixtime() - dcf77.unixtime() > 5)) {
+    rtc.adjust(dcf77);
+    Serial.println("\nNew RTC time");
+  } else {
+    Serial.println("\nRTC up to date");
+  }
+}
+
+void Serial_print99(uint8_t nombre) {
+  if (nombre < 10) Serial.print('0');
+  Serial.print(nombre);
+}
+
+
+uint16_t getNextPos(uint8_t digit, uint8_t servo, uint8_t aiguille) {
   uint16_t dest = digitPWM[digit][servo][aiguille];
   uint16_t cur = digitPWMCurrent[digit][servo][aiguille];
   uint16_t step = 1;
-  if(dest == cur){
+  if (dest == cur) {
     return cur;
-  } else if(dest > cur){
-    digitPWMCurrent[digit][servo][aiguille] += min(dest-cur,step);
+  } else if (dest > cur) {
+    digitPWMCurrent[digit][servo][aiguille] += min(dest - cur, step);
   } else {
-    digitPWMCurrent[digit][servo][aiguille] += max(dest-cur,-step);
+    digitPWMCurrent[digit][servo][aiguille] += max(dest - cur, -step);
   }
   return digitPWMCurrent[digit][servo][aiguille];
 }
 
-void displayDigit(uint8_t screen,uint8_t digit){
+void displayDigit(uint8_t screen, uint8_t digit) {
   //For each servo move 1 forward
-  for(uint16_t s = 0; s < 6 ;s++){
-    for(uint16_t a = 0; a < 2 ;a++){
+  for (uint16_t s = 0; s < 6; s++) {
+    for (uint16_t a = 0; a < 2; a++) {
       //aiguille
       pwm[screenConfig[screen][s][a][0]].setPWM(screenConfig[screen][s][a][1], 0, getNextPos(digit, s, a));
     }
   }
 }
 
-void displayTime(uint8_t h1,uint8_t h2,uint8_t m1,uint8_t m2){
-  displayDigit(0,h1);
-  displayDigit(1,h2);
-  displayDigit(2,m1);
-  displayDigit(3,m2);
+void displayTime(uint8_t h1, uint8_t h2, uint8_t m1, uint8_t m2) {
+  displayDigit(0, h1);
+  displayDigit(1, h2);
+  displayDigit(2, m1);
+  displayDigit(3, m2);
 }
 
 void loop() {
-  // Drive each servo one at a time using setPWM()
-  Serial.println(servonum);
+  static uint8_t longueur = 0;
 
-  displayTime(0,0,0,0);
+  bool trame_decodee = decodeurDCF77.traiterSignal(digitalRead(PIN_DCF77), millis());
 
-/*
-  for (uint16_t pulselen = SERVOMIN; pulselen < SERVOMAX; pulselen++) {
-    for (uint16_t num = 0; num<16; num++) {
-      pwm.setPWM(num, 0, pulselen);
-    }
+  if (trame_decodee) {
+    Serial.print(' ');
+    Serial_printDCF77();
   }
 
-  delay(500);
-  for (uint16_t pulselen = SERVOMAX; pulselen > SERVOMIN; pulselen--) {
-    for (uint16_t num = 0; num<16; num++) {
-      pwm.setPWM(num, 0, pulselen);
-    }
+  if (longueur > decodeurDCF77.longueur_trame_en_cours()) {
+    longueur = 0;
+    Serial.println();
   }
-  */
 
-
-
-  delay(500);
-
-  servonum++;
-  if (servonum > 7) servonum = 0; // Testing the first 8 servo channels
+  while (longueur < decodeurDCF77.longueur_trame_en_cours()) {
+    Serial.print(decodeurDCF77.bit_trame(longueur++));
+    //displayTime();
+  }
+  DateTime now = rtc.now();
+  int hh = now.hour();
+  int mm = now.minute();
+  displayTime(hh - (hh % 10), hh % 10, mm - (mm % 10), mm % 10);
 }
