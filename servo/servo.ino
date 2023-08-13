9bce0b6ce2b02bbae14ea416c6f4322eba69dfc3
Alexandre Wetzel <alexwtz@gmail.com>
2023-08-13 16:04:08 +0200

    code arduino

--- servo/servo.ino
+++ servo/servo.ino
@@ -21,7 +21,139 @@
 #include <Adafruit_PWMServoDriver.h>
 
 // called this way, it uses the default address 0x40
-Adafruit_PWMServoDriver pwm = Adafruit_PWMServoDriver();
+Adafruit_PWMServoDriver pwm1 = Adafruit_PWMServoDriver(0x40);
+Adafruit_PWMServoDriver pwm2 = Adafruit_PWMServoDriver(0x41);
+Adafruit_PWMServoDriver pwm3 = Adafruit_PWMServoDriver(0x42);
+Adafruit_PWMServoDriver pwm[] = {pwm1,pwm2,pwm3};
+
+uint16_t digitPWMCurrent[10][6][2];
+uint16_t digitPWM[10][6][2]={
+//0
+{
+  //servo0 -> {aiguille0, aiguille2}
+  {0,0},
+  //servo1
+  {0,0},
+  {0,0},
+  {0,0},
+  {0,0},
+  {0,0}
+},
+//1
+{
+  {0,0},
+  {0,0},
+  {0,0},
+  {0,0},
+  {0,0},
+  {0,0}  
+},
+//2
+{
+  {0,0},
+  {0,0},
+  {0,0},
+  {0,0},
+  {0,0},
+  {0,0}  
+},
+//3
+{
+  {0,0},
+  {0,0},
+  {0,0},
+  {0,0},
+  {0,0},
+  {0,0}  
+},
+//4
+{
+  {0,0},
+  {0,0},
+  {0,0},
+  {0,0},
+  {0,0},
+  {0,0}  
+},
+//5
+{
+  {0,0},
+  {0,0},
+  {0,0},
+  {0,0},
+  {0,0},
+  {0,0}  
+},
+//6
+{
+  {0,0},
+  {0,0},
+  {0,0},
+  {0,0},
+  {0,0},
+  {0,0}  
+},
+//7
+{
+  {0,0},
+  {0,0},
+  {0,0},
+  {0,0},
+  {0,0},
+  {0,0}  
+},
+//8
+{
+  {0,0},
+  {0,0},
+  {0,0},
+  {0,0},
+  {0,0},
+  {0,0}  
+},
+//9
+{
+  {0,0},
+  {0,0},
+  {0,0},
+  {0,0},
+  {0,0},
+  {0,0}  
+}
+};
+
+uint8_t screenConfig[4][6][2][2]={
+  {
+    {{0,0},{0,1}},
+    {{0,2},{0,3}},
+    {{0,4},{0,5}},
+    {{0,6},{0,7}},
+    {{0,8},{0,9}},
+    {{0,10},{0,11}}
+  },{
+    {{0,12},{0,13}},
+    {{0,14},{0,15}},
+    {{1,0},{1,1}},
+    {{1,2},{1,3}},
+    {{1,4},{1,5}},
+    {{1,6},{1,7}}
+  },{
+    {{1,8},{1,9}},
+    {{1,10},{1,11}},
+    {{1,12},{1,13}},
+    {{1,14},{1,15}},
+    {{2,2},{2,3}},
+    {{2,0},{2,1}},
+  },{
+    {{2,4},{2,5}},
+    {{2,6},{2,7}},
+    {{2,8},{2,9}},
+    {{2,10},{2,11}},
+    {{2,12},{2,13}},
+    {{2,14},{2,15}}
+  }
+};
+
 // you can also call it with a different address you want
 //Adafruit_PWMServoDriver pwm = Adafruit_PWMServoDriver(0x41);
 // you can also call it with a different address and I2C interface
@@ -44,48 +176,68 @@ void setup() {
   Serial.begin(9600);
   Serial.println("8 channel Servo test!");
 
-  pwm.begin();
-  /*
-   * In theory the internal oscillator (clock) is 25MHz but it really isn't
-   * that precise. You can 'calibrate' this by tweaking this number until
-   * you get the PWM update frequency you're expecting!
-   * The int.osc. for the PCA9685 chip is a range between about 23-27MHz and
-   * is used for calculating things like writeMicroseconds()
-   * Analog servos run at ~50 Hz updates, It is importaint to use an
-   * oscilloscope in setting the int.osc frequency for the I2C PCA9685 chip.
-   * 1) Attach the oscilloscope to one of the PWM signal pins and ground on
-   *    the I2C PCA9685 chip you are setting the value for.
-   * 2) Adjust setOscillatorFrequency() until the PWM update frequency is the
-   *    expected value (50Hz for most ESCs)
-   * Setting the value here is specific to each individual I2C PCA9685 chip and
-   * affects the calculations for the PWM update frequency. 
-   * Failure to correctly set the int.osc value will cause unexpected PWM results
-   */
-  pwm.setOscillatorFrequency(27000000);
-  pwm.setPWMFreq(SERVO_FREQ);  // Analog servos run at ~50 Hz updates
-
+  for(uint8_t i = 0; i++; i<3){
+    pwm[i].begin();
+    /*
+    * In theory the internal oscillator (clock) is 25MHz but it really isn't
+    * that precise. You can 'calibrate' this by tweaking this number until
+    * you get the PWM update frequency you're expecting!
+    * The int.osc. for the PCA9685 chip is a range between about 23-27MHz and
+    * is used for calculating things like writeMicroseconds()
+    * Analog servos run at ~50 Hz updates, It is importaint to use an
+    * oscilloscope in setting the int.osc frequency for the I2C PCA9685 chip.
+    * 1) Attach the oscilloscope to one of the PWM signal pins and ground on
+    *    the I2C PCA9685 chip you are setting the value for.
+    * 2) Adjust setOscillatorFrequency() until the PWM update frequency is the
+    *    expected value (50Hz for most ESCs)
+    * Setting the value here is specific to each individual I2C PCA9685 chip and
+    * affects the calculations for the PWM update frequency. 
+    * Failure to correctly set the int.osc value will cause unexpected PWM results
+    */
+    pwm[i].setOscillatorFrequency(27000000);
+    pwm[i].setPWMFreq(SERVO_FREQ);  // Analog servos run at ~50 Hz updates
+  }
   delay(10);
 }
 
-// You can use this function if you'd like to set the pulse length in seconds
-// e.g. setServoPulse(0, 0.001) is a ~1 millisecond pulse width. It's not precise!
-void setServoPulse(uint8_t n, double pulse) {
-  double pulselength;
-  
-  pulselength = 1000000;   // 1,000,000 us per second
-  pulselength /= SERVO_FREQ;   // Analog servos run at ~60 Hz updates
-  Serial.print(pulselength); Serial.println(" us per period"); 
-  pulselength /= 4096;  // 12 bits of resolution
-  Serial.print(pulselength); Serial.println(" us per bit"); 
-  pulse *= 1000000;  // convert input seconds to us
-  pulse /= pulselength;
-  Serial.println(pulse);
-  pwm.setPWM(n, 0, pulse);
+uint16_t getNextPos(uint8_t digit, uint8_t servo, uint8_t aiguille){
+  uint16_t dest = digitPWM[digit][servo][aiguille];
+  uint16_t cur = digitPWMCurrent[digit][servo][aiguille];
+  uint16_t step = 1;
+  if(dest == cur){
+    return cur;
+  } else if(dest > cur){
+    digitPWMCurrent[digit][servo][aiguille] += min(dest-cur,step);
+  } else {
+    digitPWMCurrent[digit][servo][aiguille] += max(dest-cur,-step);
+  }
+  return digitPWMCurrent[digit][servo][aiguille];
+}
+
+void displayDigit(uint8_t screen,uint8_t digit){
+  //For each servo move 1 forward
+  for(uint16_t s = 0; s < 6 ;s++){
+    for(uint16_t a = 0; a < 2 ;a++){
+      //aiguille
+      pwm[screenConfig[screen][s][a][0]].setPWM(screenConfig[screen][s][a][1], 0, getNextPos(digit, s, a));
+    }
+  }
+}
+
+void displayTime(uint8_t h1,uint8_t h2,uint8_t m1,uint8_t m2){
+  displayDigit(0,h1);
+  displayDigit(1,h2);
+  displayDigit(2,m1);
+  displayDigit(3,m2);
 }
 
 void loop() {
   // Drive each servo one at a time using setPWM()
   Serial.println(servonum);
+
+  displayTime(0,0,0,0);
+
+/*
   for (uint16_t pulselen = SERVOMIN; pulselen < SERVOMAX; pulselen++) {
     for (uint16_t num = 0; num<16; num++) {
       pwm.setPWM(num, 0, pulselen);
@@ -98,6 +250,7 @@ void loop() {
       pwm.setPWM(num, 0, pulselen);
     }
   }
+  */
 
 
 
